1cd1cb441f1cedf3b0e929b61c211973cc27fae1
Jack Kinsey <jack.w.kinsey@gmail.com>
2023-11-18 13:49:30 -0500

    chore: displays temporary content to "park" site

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,40 +2,23 @@ import type { MetaFunction } from '@vercel/remix'
 
 export const meta: MetaFunction = () => {
 	return [
-		{ title: 'New Remix App' },
-		{ name: 'description', content: 'Welcome to Remix!' },
+		{ title: 'Jack Kinsey' },
+		{
+			name: 'description',
+			content: 'The portfolio and resume website for Jack Kinsey',
+		},
 	]
 }
 
 export default function Index() {
 	return (
 		<div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
-			<h1>Welcome to Remix</h1>
-			<ul>
-				<li>
-					<a
-						target="_blank"
-						href="https://remix.run/tutorials/blog"
-						rel="noreferrer"
-					>
-						15m Quickstart Blog Tutorial
-					</a>
-				</li>
-				<li>
-					<a
-						target="_blank"
-						href="https://remix.run/tutorials/jokes"
-						rel="noreferrer"
-					>
-						Deep Dive Jokes App Tutorial
-					</a>
-				</li>
-				<li>
-					<a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-						Remix Docs
-					</a>
-				</li>
-			</ul>
+			<h1>Hello, I'm Jack</h1>
+			<h2>A Full Stack Developer</h2>
+
+			<p>
+				This is the future home of my portfolio website. Please check back soon!
+			</p>
 		</div>
 	)
 }
