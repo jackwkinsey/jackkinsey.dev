80c7571c77a65c55efe510e95e9c9023bb21ab4a
Jack Kinsey <jack.w.kinsey@gmail.com>
2023-11-22 17:10:26 -0600

    chore: adds fonts and styles

--- app/root.tsx
+++ app/root.tsx
@@ -9,8 +9,11 @@ import {
 	ScrollRestoration,
 } from '@remix-run/react'
 
+import sharedStyles from '~/styles/main.css'
+
 export const links: LinksFunction = () => [
 	...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
+	{ rel: 'stylesheet', href: sharedStyles },
 ]
 
 export default function App() {
@@ -19,6 +22,16 @@ export default function App() {
 			<head>
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link
+					rel="preconnect"
+					href="https://fonts.gstatic.com"
+					crossOrigin="anonymous"
+				/>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Josefin+Sans&family=Open+Sans&family=Source+Code+Pro&display=swap"
+					rel="stylesheet"
+				/>
 				<Meta />
 				<Links />
 			</head>
