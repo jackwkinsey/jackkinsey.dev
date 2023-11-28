281477969f4bb3be89ad93e59f7385cfffc809c4
Jack Kinsey <jack.w.kinsey@gmail.com>
2023-11-27 23:20:36 -0500

    chore: adds custom fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,17 @@
 import type { Config } from 'tailwindcss'
+import defaultTheme from 'tailwindcss/defaultTheme'
 
 export default {
 	content: ['./app/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {},
+		},
+		fontFamily: {
+			mono: ['"Source Code Pro"', ...defaultTheme.fontFamily.mono],
+			sans: ['"Open Sans"', ...defaultTheme.fontFamily.sans],
+			serif: ['"Josefin Sans"', ...defaultTheme.fontFamily.serif],
+		},
 	},
 	plugins: [],
 } satisfies Config
